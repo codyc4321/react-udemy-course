2ff7d7292b220a7972dbdde8d232aa0c4cd29191
Cody Childers <cchilder@mail.usf.edu>
2022-07-27 13:18:39 -0400

    finishing images ui

--- section_7_user_input_forms_and_events/src/components/App.js
+++ section_7_user_input_forms_and_events/src/components/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 
+import ImageList from './ImageList';
 import SearchBar from './SearchBar';
-
-import axios from 'axios';
+import '../api/unsplash';
+// import axios from 'axios';
 
 
 class App extends React.Component {
 
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = {images: []};
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
       params: {query: term},
-      headers: {
-        Authorization: 'Client-ID *YOUR KEY HERE*'
-      }
     });
+
+    console.log(response.data.results);
+    this.setState({images: response.data.results});
   }
 
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList />
       </div>
     )
   }
