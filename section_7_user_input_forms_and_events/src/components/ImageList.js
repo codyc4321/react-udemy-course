5fb46f18ef0099892bec3950cbc86467122c2039
Cody Childers <cchilder@mail.usf.edu>
2022-07-28 23:09:10 -0400

    added func to display unsplash images

--- section_7_user_input_forms_and_events/src/components/ImageList.js
+++ section_7_user_input_forms_and_events/src/components/ImageList.js
@@ -10,4 +10,14 @@ const ImageList = props => {
   return <div>{images}</div>;
 };
 
+// const ImageList = props => {
+//
+//   const images = props.images.map((image) => {
+//     return <img src={image.urls.regular} />
+//   })
+// 
+//   return <div>{images}</div>;
+// };
+
+
 export default ImageList;
