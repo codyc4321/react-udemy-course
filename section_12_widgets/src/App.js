b7a1334d43e6b205a32f9dd6407852415c16987f
Cody Childers <cchilder@mail.usf.edu>
2022-07-31 11:25:02 -0400

    added search bar

--- section_12_widgets/src/App.js
+++ section_12_widgets/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Accordion from './components/Accordion';
+import Search from './components/Search';
+
 
 const items = [
   {title: 'What is React?', content: 'React is a front end javascript framework'},
@@ -12,7 +14,7 @@ export default () => {
   return (
     <div>
       <br/>
-      <Accordion items={items} />
+      <Search />
     </div>
   )
 }
