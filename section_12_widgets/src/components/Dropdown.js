ecdd3f42957a3cd6fc8ca8fe229756a4858fbf73
Cody <cchilder@mail.usf.edu>
2022-08-01 12:14:52 -0400

    set dropdown to close when label is clicked

--- section_12_widgets/src/components/Dropdown.js
+++ section_12_widgets/src/components/Dropdown.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
 
+  const [open, setOpen] = useState(false);
+
   const renderedOptions = options.map((option) => {
+
+    if(option.value === selected.value) {
+      return null;
+    }
+
     return (
       <div
         className="item"
@@ -16,10 +23,12 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open? 'visible active' : ''}`}>
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
             {renderedOptions}
           </div>
         </div>
