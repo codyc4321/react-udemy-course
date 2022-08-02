739dfd9c0129c827d7e72502cc044604023b993d
Cody <cchilder@mail.usf.edu>
2022-08-02 10:30:59 -0400

    finished dropdown widget working on translate

--- section_12_widgets/src/App.js
+++ section_12_widgets/src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 
 const items = [
@@ -28,14 +29,11 @@ const options = [
 
 export default () => {
   const [selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
-      <br/>
-      <Dropdown
-        selected={selected}
-        options={options}
-        onSelectedChange={setSelected} />
+      <Translate />
     </div>
   )
 }
