6b395c773192ee34c992331a219bcc08a9910f83
Cody <cchilder@mail.usf.edu>
2022-08-06 14:29:04 -0400

    configure connect and map state

---
+++ section_17_songs/src/components/SongList.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+
+class SongList extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>Songlist</div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log(state);
+
+  return {songs: state.songs};
+}
+
+export default connect(mapStateToProps)(SongList);
