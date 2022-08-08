2b6bc3241cb281275734ac3a1b568c194a5d8058
Cody <cchilder@mail.usf.edu>
2022-08-08 01:19:17 -0400

    got request for users working

--- section_18_blog_posts/src/actions/index.js
+++ section_18_blog_posts/src/actions/index.js
@@ -5,3 +5,10 @@ export const fetchPosts = () => async dispatch => {
   const response = await json_placeholder.get('/posts');
   dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
+
+
+export const fetchUser = (id) => async dispatch => {
+  const response = await json_placeholder.get(`/users/${id}`);
+
+  dispatch({type: 'FETCH_USER', payload: response.data});
+}
