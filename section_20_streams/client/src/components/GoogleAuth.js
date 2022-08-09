45485d375f0b748239c910bb196978e7f9404277
Cody <cchilder@mail.usf.edu>
2022-08-09 17:03:29 -0400

    app broken

--- section_20_streams/client/src/components/GoogleAuth.js
+++ section_20_streams/client/src/components/GoogleAuth.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {signIn, signOut} from '../actions';
+
 
 class GoogleAuth extends React.Component {
-
-  state = {isSignedIn: null};
 
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
@@ -12,14 +13,20 @@ class GoogleAuth extends React.Component {
         plugin_name: 'streaming'
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
-        this.setState({isSignedIn: this.auth.isSignedIn.get()});
+        // this.setState({isSignedIn: this.auth.isSignedIn.get()});
+        this.onAuthChange(this.auth.isSignedIn.get())
         this.auth.isSignedIn.listen(this.onAuthChange);
       })
     });
   }
 
-  onAuthChange = () => {
-    this.setState({isSignedIn: this.auth.isSignedIn.get()});
+  onAuthChange = (isSignedIn) => {
+    // this.setState({isSignedIn: this.auth.isSignedIn.get()});
+    if (isSignedIn) {
+      this.props.signIn();
+    } else {
+      this.props.signOut();
+    }
   }
 
   onSignInClick = () => {
@@ -58,4 +65,9 @@ class GoogleAuth extends React.Component {
 }
 
 
-export default GoogleAuth;
+const mapStateToProps = (state) => {
+  return {isSignedIn: state.auth.isSignedIn}
+}
+
+
+export default connect(mapStateToProps, {signIn, signOut})(GoogleAuth);
